54db317008abaacb91be3c36dd5824f5cc9a6dce
marcius-llmus <asyncmarcio@gmail.com>
2026-01-27 17:29:16 -0500

    Add build messaging turn with turn dep

--- app/coder/factories.py
+++ app/coder/factories.py
@@ -1,3 +1,4 @@
+from app.chat.schemas import Turn
 from app.coder.services.messaging import MessagingTurnEventHandler
 from app.coder.services.single_shot_patching import SingleShotPatchService
 from app.context.factories import build_workspace_service
@@ -5,8 +6,10 @@ from app.core.db import sessionmanager
 from app.patches.factories import build_diff_patch_service
 
 
-async def build_messaging_turn_event_handler() -> MessagingTurnEventHandler:
-    return MessagingTurnEventHandler()
+async def build_messaging_turn_event_handler(
+    *, turn: Turn
+) -> MessagingTurnEventHandler:
+    return MessagingTurnEventHandler(turn=turn)
 
 
 async def build_single_shot_patch_service() -> SingleShotPatchService:
