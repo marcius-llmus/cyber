4f4fe68e8d3dfbeb139d48a8713b9b8921247011
marcius-llmus <asyncmarcio@gmail.com>
2026-02-02 17:37:39 -0500

    Remove tests safe mask in update

--- tests/llms/test_services.py
+++ tests/llms/test_services.py
@@ -9,7 +9,6 @@ from app.llms.exceptions import (
 )
 from app.llms.models import LLMSettings
 from app.llms.schemas import LLM
-from app.settings.constants import API_KEY_MASK
 from app.settings.exceptions import (
     ContextWindowExceededException,
     LLMSettingsNotFoundException,
@@ -220,25 +219,6 @@ async def test_llm_service__update_settings__updates_api_key_for_provider_when_a
 
     llm_service.llm_settings_repo.update_api_key_for_provider.assert_awaited_once()
 
-
-async def test_llm_service__update_settings__does_not_overwrite_when_mask_placeholder_sent(
-    llm_settings_openai_no_role_mock,
-    llm_service,
-):
-    llm_service.llm_settings_repo.get = AsyncMock(
-        return_value=llm_settings_openai_no_role_mock
-    )
-    llm_service.llm_settings_repo.update = AsyncMock(
-        return_value=llm_settings_openai_no_role_mock
-    )
-    llm_service.llm_settings_repo.update_api_key_for_provider = AsyncMock()
-
-    await llm_service.update_settings(
-        llm_settings_openai_no_role_mock.id,
-        LLMSettingsUpdate(api_key=API_KEY_MASK),
-    )
-
-    llm_service.llm_settings_repo.update_api_key_for_provider.assert_not_awaited()
 
 
 async def test_llm_service__update_coding_llm__sets_role_then_updates_settings(
