ff5047d4d916d7b0f0b46fe87f367e563cb29a41
marcius-llmus <asyncmarcio@gmail.com>
2026-02-04 07:38:18 -0500

    Add factory for turn execution

--- app/coder/factories.py
+++ app/coder/factories.py
@@ -1,6 +1,7 @@
 from app.chat.schemas import Turn
 from app.coder.services.messaging import MessagingTurnEventHandler
 from app.coder.services.single_shot_patching import SingleShotPatchService
+from app.coder.services.execution_registry import TurnExecutionRegistry
 from app.context.factories import build_workspace_service
 from app.core.db import sessionmanager
 from app.patches.factories import build_diff_patch_service
@@ -18,3 +19,7 @@ async def build_single_shot_patch_service() -> SingleShotPatchService:
         diff_patch_service_factory=build_diff_patch_service,
         context_service_factory=build_workspace_service,
     )
+
+
+def build_turn_execution_registry() -> TurnExecutionRegistry:
+    return TurnExecutionRegistry()
