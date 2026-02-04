ee92ecaf486a05a5a45a56ab886e01076e9804ee
marcius-llmus <asyncmarcio@gmail.com>
2026-02-04 08:01:09 -0500

    Add turn execution registry related services to init

--- app/coder/services/__init__.py
+++ app/coder/services/__init__.py
@@ -2,10 +2,16 @@ from .coder import CoderService
 from .messaging import MessagingTurnEventHandler
 from .page import CoderPageService
 from .single_shot_patching import SingleShotPatchService
+from .execution_registry import TurnExecutionRegistry
+from .execution_registry import initialize_global_registry
+from .execution_registry import get_global_registry
 
 __all__ = [
     "CoderService",
     "MessagingTurnEventHandler",
     "CoderPageService",
     "SingleShotPatchService",
+    "TurnExecutionRegistry",
+    "initialize_global_registry",
+    "get_global_registry",
 ]
